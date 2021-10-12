3b42b342d2cbd85154d8cfaaa5995fbb4cba4aea
Tanner Daria <89423851+tannerdaria@users.noreply.github.com>
2021-10-12 01:26:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Main {
+  public static void main(String[] args) {    
+    Student student1 = new Student();
+    
+    student1.setName("Arnold Spielberg");    student1.setGPA(4.0);    
+    student1.setMajor("Electrical Engineering");
+
+    Student student2 = new Student();
+
+    student2.setName("Tanner Daria");    
+    student2.setGPA(4.3);    
+    student2.setMajor("Business Analytics");
+    
+    // or can do this with the dot operator    //student1.name = "Arnold Spielberg";    //student1.gpa = 4.0;    
+    //student1.major = "Electrical Engineering";    
+    
+    student1.print();
+    student2.print();
+
+    ArrayList<Double> gpas = new ArrayList<Double>();
+    gpas.add(student1.gpa);
+    gpas.add(student2.gpa);
+
+    double sum = 0;
+
+    for(int i=0; i<gpas.size(); i++){
+      sum += gpas.get(i);
+    }
+
+    System.out.println("The average GPA is " + sum/gpas.size());
+  
+  }
+}
